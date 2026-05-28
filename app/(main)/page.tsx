cf4dbc50a95da6f7aec259204ea0d1941b7228f6
Emmy <famuyiwaemmanuel565@gmail.com>
2026-05-28 17:51:56 +0100

    another new changes

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <h1>Event Management</h1>;
-}
+import Link from "next/link";
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>ESRMS</h1>
+      <h2>Manage Events Without the Chaos</h2>
+      <h3>
+        If you do not have an account{" "}
+        <span>
+          <Link href="sign-up">Sign Up</Link>
+        </span>
+        , or if you have an account{" "}
+        <span>
+          <Link href="sign-in">Sign In</Link>
+        </span>
+      </h3>
+    </div>
+  );
+};
+
+export default MainPage;
