18d951824ffa86f84ad7f81f4c773c40ca3ed429
Emmy <famuyiwaemmanuel565@gmail.com>
2026-06-01 00:16:54 +0100

    building in progress

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -6,6 +6,7 @@ import SignInText from "@/components/SignInText";
 import SignUpForm from "@/components/SignUpForm";
 import SignUpText from "@/components/SignUpText";
 import { createClient } from "@/utils/supabase/client";
+import { Calendar } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -68,7 +69,35 @@ const MainPage = () => {
     }
   };
 
-  return <main className="grid grid-cols-2"></main>;
+  return (
+    <main className="min-h-screen flex">
+      <div className="hidden lg:flex lg-w-[55%] sidebar flex-col justify-betweenp-12 relative overflow-hidden">
+        <div
+          className="absolute inset-0 opacity-0.04"
+          style={{
+            backgroundImage:
+              "linear-gradient(#fff 1px,transparent 1px) linear-gradient(90deg,#fff 1px,transparent 1px)",
+            backgroundSize: "40px 40px",
+          }}
+        />
+        <div
+          className="absolute top-1/3 -left-20 w-96 h-96 rounded-full opacity-10"
+          style={{
+            background: "radial-gradient(circle, #F59E0B 0%, transparent-70%",
+          }}
+        />
+
+        <div className="relative z-10 flex items-center gap-3">
+          <div className="w-10 h-10 rounded-xl bg-amber-500 flex items-center justify-center">
+            <Calendar className="w-5 h-5 text-white" />
+          </div>
+          <span className="font-bold text-white text-xl font-family-display">
+            ESRMS
+          </span>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default MainPage;
