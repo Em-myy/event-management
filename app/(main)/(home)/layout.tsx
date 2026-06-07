19e21872ae28ff053d23149f030b1c596d73abe1
Emmy <famuyiwaemmanuel565@gmail.com>
2026-06-07 23:04:14 +0100

    new changes in layout

--- app/(main)/(home)/layout.tsx
+++ app/(main)/(home)/layout.tsx
@@ -1,14 +1,32 @@
-import NavBar from "@/components/Sidebar";
+import Sidebar from "@/components/Sidebar";
+import { useAuth } from "@/context/AuthContext";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function AppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  if (!user) redirect("/");
+
+  const { data: profile } = await supabase
+    .from("profiles")
+    .select("*, roles(name, label)")
+    .eq("id", user.id)
+    .single();
+
   return (
-    <div className="flex justify-between p-4">
-      <NavBar />
-      {children}
+    <div className="flex h-screen overflow-hidden bg-slate-100">
+      <Sidebar profile={profile} />
+      <main className="flex-1 overflow-y-auto">
+        <div className="max-w-7xl mx-auto px-6 py-8">{children}</div>
+      </main>
     </div>
   );
 }
